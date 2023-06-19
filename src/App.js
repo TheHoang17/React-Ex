d940ab8e30e89d6012b3629775515ad397117f4c
The Hoang <thehoang1722@gmail.com>
2023-06-20 00:02:43 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import React, {Component} from 'react';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
-
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
  
   render (){
 return (
-    <div className="App">
-      <Main/>  </div>
+  <BrowserRouter>
+  <div className="App">
+    <Main />
+  </div>
+</BrowserRouter>
+
   );
 }
 }
